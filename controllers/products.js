50bf55087a57404d4ea8beab78eeee8b9c770ea7
Prokopevs <prokopev_savva@mail.ru>
2023-03-09 21:08:52 +0300

    query params

--- controllers/products.js
+++ controllers/products.js
@@ -16,24 +16,44 @@ const getAllProductsStatic = async (req, res) => {
         sqlReq = [product_company_name]
     }
 
-    console.log(sqlQuery)
     const products = await db.query(sqlQuery, sqlReq)
-
     res.status(200).json({ data: products.rows, nbHits: products.rows.length });
 };
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: "products route" });
+    const { product_featured, product_company_name, product_name, sort, fields, numericFilters } = req.query;
+    let sqlQuery = `SELECT * FROM products`
+
+    const createSQLString = (queryKey, queryValue, queryType) => {
+        const checkQueryType = (queryValue, queryType) => {
+            return queryType === "string" ? `'${queryValue}'`: `${queryValue}`
+        }
+
+        if (/WHERE/.test(sqlQuery)) {
+            sqlQuery = sqlQuery + ` AND ${queryKey} = ${checkQueryType(queryValue, queryType)}`
+        } else {
+            sqlQuery = sqlQuery + ` WHERE ${queryKey} = ${checkQueryType(queryValue, queryType)}`
+        }
+    }  
+    
+
+    if (product_featured) {
+        createSQLString("product_featured", product_featured)
+        console.log(sqlQuery)
+    }
+    if (product_company_name) {
+        createSQLString("product_company_name", product_company_name, "string")
+        console.log(sqlQuery)
+    }
+    if (product_name) {
+        createSQLString("product_name", product_name, "string")
+        console.log(sqlQuery)
+    }
+
+
+    const products = await db.query(sqlQuery)
+    res.status(200).json({ data: products.rows, nbHits: products.rows.length });
 };
-
-
-// const getAllProductsStatic = async (req, res) => {
-//   const products = await Product.find({ price: { $gt: 30 } })
-//     .sort('price')
-//     .select('name price');
-
-//   res.status(200).json({ products, nbHits: products.length });
-// };
 
 // const getAllProducts = async (req, res) => {
 //   const { featured, company, name, sort, fields, numericFilters } = req.query;
